efc3a0e7320630c63f2d6b8afd87c27fe5cc02c3
Dmytro Bevzenko <dbvz@protonmail.com>
2021-07-14 21:28:48 -0500

    Add visitor and refactored code to use it.

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -8,23 +8,42 @@ fn name() -> String {
     your_name.trim().to_lowercase()
 }
 
+struct Visitor {
+    name: String,
+    greeting: String,
+}
+
+impl Visitor {
+    fn new(name: &str, greeting: &str) -> Self {
+        Self {
+            // note that here we create new string and copy name into it
+            name: name.to_lowercase(),
+            greeting: greeting.to_string(),
+        }
+    }
+
+    fn greet_visitor(&self) {
+        println!("{}", self.greeting);
+    }
+}
+
 fn main() {
-    let visitors = ["bert", "steve", "fred"];
+    let visitors = [
+        Visitor::new("bert", "Hello Bert, enjoy your treehouse."),
+        Visitor::new("steve", "Hi, Steve. Your milk is in the fridge."),
+        Visitor::new("fred", "Wow, who invited Fred?"),
+    ];
 
     println!("Hello, what is your name?");
 
     let name = name();
-    let mut allow_in = false;
 
-    for visitor in &visitors {
-        if visitor == &name {
-            allow_in = true;
-        }
-    }
+    let visitor = visitors
+        .iter()
+        .find(|visitor| visitor.name == name);
 
-    if allow_in {
-        println!("Welcome, {}", name);
-    } else {
-        println!("Sorry, you are not on the list.")
+    match visitor {
+        Some(visitor) => visitor.greet_visitor(),
+        None => println!("Sorry, you are not on the list. Please leave.")
     }
 }
