318f1cce77bdd8eb1411e7e666d1c0f8880e4e47
Nazar Bevzenko <nazarbevzenko@gmail.com>
2021-07-20 13:46:25 -0700

    Formatted variable and allowed only "bert".

--- treehouse/src/main.rs
+++ treehouse/src/main.rs
@@ -7,10 +7,16 @@ fn what_is_your_name() -> String {
         .read_line(&mut your_name)
         .expect("Failed to read line");
     your_name
+        .trim()
+        .to_lowercase()
 }
 
 fn main() {
     println!("Hello, what's your name?");
     let name = what_is_your_name();
-    println!("Hello, {}", name);
+    if name == "bert" {
+        println!("Welcome.");
+    } else {
+        println!("Sorry, you are not on the list.");
+    }
 }
