a6b8afd8d6a02d0b8545e9eb08284a1c5d8775c0
Dmytro Bevzenko <dbvz@protonmail.com>
2021-07-20 23:42:47 -0500

    Add player

--- flappy/src/main.rs
+++ flappy/src/main.rs
@@ -6,27 +6,81 @@ enum GameMode {
     End,
 }
 
-struct State {
-    mode: GameMode
+const SCREEN_WIDTH : i32 = 80;
+const SCREEN_HEIGHT : i32 = 50;
+const FRAME_DURATION : f32 = 75.0;
+
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
 }
 
-impl State {
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
 
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK, to_cp437('@'))
+    }
+
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0 {
+            self.y = 0;
+        }
+    }
+
+    fn flap(&mut self) {
+        self.velocity = -2.0
+    }
+}
+
+struct State {
+    player: Player,
+    frame_time: f32,
+    mode: GameMode,
 }
 
 impl State {
     fn new() -> Self {
         State {
+            player: Player::new(5,25),
+            frame_time: 0.0,
             mode: GameMode::Menu,
         }
     }
 
     fn play(&mut self, ctx: &mut BTerm) {
-        // todo: Fill in the stub later
-        self.mode = GameMode::End;
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0,0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGHT {
+            self.mode = GameMode::End;
+        }
+
     }
 
     fn restart(&mut self) {
+        self.player = Player::new(5,25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
 
@@ -63,8 +117,8 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.dead(ctx),
-            GameMode::End => self.play(ctx)
+            GameMode::End => self.dead(ctx),
+            GameMode::Playing => self.play(ctx),
         }
     }
 }
@@ -75,6 +129,4 @@ fn main() -> BError {
         .build()?;
 
     main_loop(context, State::new())
-
-
 }
